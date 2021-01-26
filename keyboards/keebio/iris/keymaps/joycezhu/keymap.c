51df1493664442a7f34ac2033d769c2149cf70a9
Joyce Zhu <joyce@quip.com>
2021-01-25 23:31:42 -0800

    FML rearrange layer order

--- keyboards/keebio/iris/keymaps/joycezhu/keymap.c
+++ keyboards/keebio/iris/keymaps/joycezhu/keymap.c
@@ -2,9 +2,10 @@
 
 
 #define _WINDOWS 0
-#define _NAV 1
+#define _CODING 1
 #define _MEDIA 2
-#define _CODING 3
+#define _NAV 3
+
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -21,10 +22,10 @@ enum custom_keycodes {
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_WINDOWS] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, LALT_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LSFT_T(KC_LBRC), KC_Z, KC_X, KC_C, LT(2,KC_V), KC_B, KC_LGUI, TO(3), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), TT(1), LCTL_T(KC_DEL), KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL)),
-  [_NAV] = LAYOUT(KC_ESC, KC_PSLS, KC_PAST, KC_PMNS, KC_DLR, KC_PERC, KC_INS, KC_HOME, KC_PGUP, KC_MINS, KC_EQL, KC_SLCK, KC_TAB, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_5, KC_DEL, KC_END, KC_PGDN, KC_LBRC, KC_RBRC, KC_PSCR, KC_CAPS, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_LBRC, KC_WBAK, KC_UP, KC_WFWD, KC_MUTE, KC_MPLY, KC_CALC, KC_LSFT, KC_P1, KC_P2, KC_P3, KC_0, KC_WREF, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPRV, KC_MNXT, KC_MAIL, TO(0), KC_LGUI, KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL)),
-  [_MEDIA] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_PSLS, KC_PAST, KC_PMNS, KC_F10, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_7, KC_8, KC_9, KC_PPLS, KC_CAPS, KC_MPRV, KC_MNXT, KC_VOLU, KC_BRIU, KC_F11, KC_ASTR, KC_LPRN, KC_4, KC_5, KC_6, KC_PEQL, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_BRID, KC_F12, TO(1), KC_WREF, KC_RPRN, KC_FIND, KC_1, KC_2, KC_3, KC_PENT, TO(0), KC_LGUI, KC_BSPC, KC_MSEL, KC_ENT, KC_0),
-  [_CODING] = LAYOUT(KC_LGUI, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, LALT_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LSFT_T(KC_LBRC), KC_Z, KC_X, KC_C, LT(2,KC_V), KC_B, LCTL_T(KC_DEL), TO(0), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), LT(1,KC_TAB), LGUI_T(KC_GRV), KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL))
+  [0] = LAYOUT(KC_LGUI, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, LALT_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LSFT_T(KC_LBRC), KC_Z, KC_X, KC_C, LT(2,KC_V), KC_B, KC_DEL, TO(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), TT(3), LCTL_T(KC_GRV), KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL)),
+  [1] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, LALT_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LSFT_T(KC_LBRC), KC_Z, KC_X, KC_C, LT(2,KC_V), KC_B, LCTL_T(KC_DEL), TO(0), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), TT(3), LGUI_T(KC_GRV), KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL)),
+  [2] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_PSLS, KC_PAST, KC_PMNS, KC_F10, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_7, KC_8, KC_9, KC_PPLS, KC_CAPS, KC_MPRV, KC_MNXT, KC_VOLU, KC_BRIU, KC_F11, KC_ASTR, KC_LPRN, KC_4, KC_5, KC_6, KC_PEQL, KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_BRID, KC_F12, TO(1), KC_WREF, KC_RPRN, KC_FIND, KC_1, KC_2, KC_3, KC_PENT, TO(0), KC_LGUI, KC_BSPC, KC_MSEL, KC_ENT, KC_0),
+  [3] = LAYOUT(KC_ESC, KC_PSLS, KC_PAST, KC_PMNS, KC_DLR, KC_PERC, KC_INS, KC_HOME, KC_PGUP, KC_MINS, KC_EQL, KC_SLCK, KC_TAB, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_5, KC_DEL, KC_END, KC_PGDN, KC_LBRC, KC_RBRC, KC_PSCR, KC_CAPS, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_LBRC, KC_WBAK, KC_UP, KC_WFWD, KC_MUTE, KC_MPLY, KC_CALC, KC_LSFT, KC_P1, KC_P2, KC_P3, KC_0, KC_WREF, KC_TRNS, TO(2), KC_LEFT, KC_DOWN, KC_RGHT, KC_MPRV, KC_MNXT, KC_MAIL, TO(0), KC_LGUI, KC_BSPC, KC_SPC, RCTL_T(KC_ENT), RALT_T(KC_EQL)),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -37,29 +38,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case LOWER:
       if (record->event.pressed) {
-        layer_on(_NAV);
-        update_tri_layer(_NAV, _MEDIA, _CODING);
+        layer_on(_CODING);
+        update_tri_layer(_CODING, _MEDIA, _NAV);
       } else {
-        layer_off(_NAV);
-        update_tri_layer(_NAV, _MEDIA, _CODING);
+        layer_off(_CODING);
+        update_tri_layer(_CODING, _MEDIA, _NAV);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_MEDIA);
-        update_tri_layer(_NAV, _MEDIA, _CODING);
+        update_tri_layer(_CODING, _MEDIA, _NAV);
       } else {
         layer_off(_MEDIA);
-        update_tri_layer(_NAV, _MEDIA, _CODING);
+        update_tri_layer(_CODING, _MEDIA, _NAV);
       }
       return false;
       break;
     case ADJUST:
       if (record->event.pressed) {
-        layer_on(_CODING);
+        layer_on(_NAV);
       } else {
-        layer_off(_CODING);
+        layer_off(_NAV);
       }
       return false;
       break;
@@ -70,7 +71,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     // Never fires for my Iris.
     if (index == 0) {
-        if (clockwise) {
+         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
@@ -85,7 +86,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
           } else {
             tap_code(KC_VOLD);
           }
-        } else if (layer <= _CODING) {
+        } else if (layer <= _NAV) {
            if (clockwise) {
             tap_code(KC_DOWN);
           } else {
