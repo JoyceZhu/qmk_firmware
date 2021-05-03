09f0efdcb1c19095e8a378ec87de80c64f7f6ecd
Joyce Zhu <joyce@quip.com>
2021-05-03 16:52:53 -0700

    Changes for ergonomics

--- keyboards/handwired/dactylx/keymaps/joycezhu/keymap.c
+++ keyboards/handwired/dactylx/keymaps/joycezhu/keymap.c
@@ -7,36 +7,34 @@
 #define _NAV 3
 
 
-#define TT_FNPD TT(_FNPD)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_dactylx(
-        KC_ESC , KC_1  , KC_2   , KC_3   , KC_4   , KC_5   , KC_EQL,       KC_MPLY, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(1),
-        KC_TAB , KC_Q  , KC_W   , KC_E   , KC_R   , KC_T   , LT(_FNPD, KC_MPRV),      LT(_FNPD, KC_MPRV), KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+        KC_ESC , KC_1  , KC_2   , KC_3   , KC_4   , KC_5   , LT(_FNPD, KC_MPRV),       LT(_FNPD, KC_MNXT), KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(1),
+        KC_TAB , KC_Q  , KC_W   , KC_E   , KC_R   , KC_T   , KC_VOLU,      KC_VOLD, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         LALT_T(KC_CAPS), KC_A  , KC_S   , KC_D   , KC_F   , KC_G   , KC_DEL,
-           KC_MINS, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+           KC_MPLY, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z  , KC_X   , KC_C   , KC_V   , KC_B,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
         // bottom row / thumb cluster
-        LCTL_T(KC_PSCR), KC_LBRC, KC_RBRC, KC_VOLU, KC_VOLD,
+        LCTL_T(KC_PSCR), KC_LBRC, KC_RBRC, KC_EQL, KC_MINS,
             /* lthumb */ LSFT_T(KC_GRV), LGUI_T(KC_BSPC) ,
             /* rthumb */ KC_SPC , LT(_NAV,KC_ENT),
         KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_RGUI,
-             KC_END , KC_HOME, KC_BSPC, KC_LGUI,
+             KC_END , KC_HOME, KC_BSPC, LGUI_T(KC_TAB),
              KC_RCTL, RALT_T(KC_TAB), KC_PGUP, KC_PGDN
     ),
 
     [_WINDOWS] = LAYOUT_dactylx(
-        KC_ESC , KC_1  , KC_2   , KC_3   , KC_4   , KC_5   , KC_EQL,       KC_MPLY, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(0),
-        KC_TAB , KC_Q  , KC_W   , KC_E   , KC_R   , KC_T   , LT(_FNPD, KC_MPRV),      LT(_FNPD, KC_MPRV), KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
+        KC_ESC , KC_1  , KC_2   , KC_3   , KC_4   , KC_5   , LT(_FNPD, KC_MPRV),       LT(_FNPD, KC_MNXT), KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , TO(0),
+        KC_TAB , KC_Q  , KC_W   , KC_E   , KC_R   , KC_T   , KC_VOLU,      KC_VOLD, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         LALT_T(KC_CAPS), KC_A  , KC_S   , KC_D   , KC_F   , KC_G   , KC_DEL,
-           KC_MINS, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+           KC_MPLY, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z  , KC_X   , KC_C   , KC_V   , KC_B,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
         // bottom row / thumb cluster
-        LGUI_T(KC_PSCR), KC_LBRC, KC_RBRC, KC_VOLU, KC_VOLD,
+        LGUI_T(KC_PSCR), KC_LBRC, KC_RBRC, KC_EQL, KC_MINS,
             /* lthumb */ LSFT_T(KC_GRV), LCTL_T(KC_BSPC) ,
             /* rthumb */ KC_SPC , LT(_NAV,KC_ENT),
         KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_RCTL,
-             KC_END , KC_HOME, KC_BSPC, KC_LCTL,
+             KC_END , KC_HOME, KC_BSPC, LCTL_T(KC_TAB),
              KC_RGUI, RALT_T(KC_TAB), KC_PGUP, KC_PGDN
     ),
 
